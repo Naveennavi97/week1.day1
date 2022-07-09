fd0aaf588b1e1936903aaa3d41186d679a15668f
Naveennavi97 <108997526+Naveennavi97@users.noreply.github.com>
2022-07-10 00:14:56 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Mobile {
+	public void makecall() {
+		System.out.println("y");
+	}
+	public void sendmsg() {
+	    
+		
+		System.out.println("hi");
+
+	}
+   public static void main(String[] args) {
+	System.out.println("This is my mobile");
+}
+
+}
